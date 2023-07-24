1214b7400f209544b1e77982947783d9ed69449e
Maaike <maaikejvp@hotmail.com>
2023-07-24 21:52:33 +0200

    restructuring + first projects

---
+++ session2/workbook1/mod01/first/first.js
@@ -0,0 +1,13 @@
+"use strict";
+// this is a comment
+/* multiline
+comment*/
+var name = "Tim";
+console.log("Hello " + name);
+
+var word, num;
+word = "banana";
+num = 12;
+num = num + 1; // num is now 13
+word = word + 1;
+console.log(word);
