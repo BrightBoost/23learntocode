faefa7af6c72217e709d1b2ad4f58238e3757386
Maaike <maaikejvp@hotmail.com>
2023-08-03 17:43:24 +0200

    restructured

--- session2/workbook2/mod03/interactiveCodeDemo/scripts/index.js
+++ session2/workbook2/mod03/interactiveCodeDemo/scripts/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.onload = function() {
+window.onload = function () {
     const estimateTotalCost = document.getElementById("estimateTotalCost");
     estimateTotalCost.onclick = onEstimateTotalCostClicked;
 };
@@ -8,49 +8,33 @@ window.onload = function() {
 function onEstimateTotalCostClicked() {
     // get variables
     const days = document.getElementById("days");
-    const options = document.querySelectorAll(".options");
-    const age = document.querySelector("input[name='age']:checked");
     const carRental = document.getElementById("carRental");
     const optionsPrice = document.getElementById("options");
     const surcharge = document.getElementById("surcharge");
     const totalDue = document.getElementById("totalDue");
-    let basicCharge = 0;
-    let optionCharges = 0;
-    let ageCharges = 0;
-    let totalDueNum = 0;
-
+    
+    if(days.value == "") {
+        alert("Please enter a valid nr for days");
+        return;
+    }
+    
     // reset output
     resetOutput();
 
-    // basic charge
+    // calculate charges
     let numDays = Number(days.value);
-    basicCharge = numDays * 29.99;
+    let basicCharge = numDays * 29.99;
+    let optionCharges = calcTotalOptions(numDays);
+    let ageCharges = calcSurcharge(basicCharge);
 
-    // careful with this though
-    // if the html order changes, you gets surprise behavior
-    if(options[0].checked) {
-        optionCharges += 3.95;
-    }
-    if(options[1].checked) {
-        optionCharges += 2.95;
-    }
-    if(options[2].checked) {
-        optionCharges += 2.95;
-    }
-
-    // surcharge - under 25
-    if(document.querySelector("input[value='yes']:checked")) {
-        ageCharges = 0.3 * basicCharge;
-    }
-    
     // total amount due
-    totalDueNum = basicCharge + ageCharges + optionCharges * numDays;
+    let totalDueNum = basicCharge + ageCharges + optionCharges;
+
+    // display result
     surcharge.innerHTML += `$${ageCharges.toFixed(2)}`;
     carRental.innerHTML += `$${basicCharge.toFixed(2)}`;
-    optionsPrice.innerHTML += `$${(optionCharges * numDays).toFixed(2)}`;
+    optionsPrice.innerHTML += `$${(optionCharges).toFixed(2)}`;
     totalDue.innerHTML += `$${totalDueNum.toFixed(2)}`;
-
-
 }
 
 function resetOutput() {
@@ -58,4 +42,30 @@ function resetOutput() {
     document.getElementById("options").innerHTML = "Options: ";
     document.getElementById("surcharge").innerHTML = "Surcharge: ";
     document.getElementById("totalDue").innerHTML = "Total Due: ";
+}
+
+function calcTotalOptions(numDays) {
+    const options = document.querySelectorAll(".options");
+    let optionCharges = 0;
+    // careful with this though
+    // if the html order changes, you gets surprise behavior
+    if (options[0].checked) {
+        optionCharges += 3.95;
+    }
+    if (options[1].checked) {
+        optionCharges += 2.95;
+    }
+    if (options[2].checked) {
+        optionCharges += 2.95;
+    }
+    return optionCharges * numDays;
+}
+
+function calcSurcharge(basicCharge) {
+    let ageCharges = 0;
+    // surcharge - under 25
+    if (document.querySelector("input[value='yes']:checked")) {
+        ageCharges = 0.3 * basicCharge;
+    }
+    return ageCharges;
 }
