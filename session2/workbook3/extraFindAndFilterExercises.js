9db394856c5af91a90ef788bf18bb52c3d6ff15b
Maaike <maaikejvp@hotmail.com>
2023-08-08 19:02:36 +0200

    extra filter

---
+++ session2/workbook3/extraFindAndFilterExercises.js
@@ -0,0 +1,26 @@
+const spongebobCharacters = [
+    { name: "SpongeBob SquarePants", job: "Fry Cook at the Krusty Krab" },
+    { name: "Patrick Star", job: "Unemployed" },
+    { name: "Squidward Tentacles", job: "Cashier at the Krusty Krab" },
+    { name: "Mr. Krabs", job: "Owner of the Krusty Krab" },
+    { name: "Sandy Cheeks", job: "Scientist and Inventor" },
+    { name: "Plankton", job: "Owner of the Chum Bucket" },
+    { name: "Mrs. Puff", job: "Boating School Teacher" },
+    { name: "Larry the Lobster", job: "Lifeguard at Goo Lagoon" },
+    { name: "Mermaid Man", job: "Retired Superhero" },
+    { name: "Barnacle Boy", job: "Retired Superhero Sidekick" },
+    { name: "Pearl Krabs", job: "High School Student" },
+    { name: "Karen", job: "Chum Bucket Computer/Wife of Plankton" }
+];
+
+
+
+// Find the Unemployed Character: Write a function that filters the `spongebobCharacters` array to find the character(s) who are unemployed. Return their name(s).
+
+// Krusty Krab Employees: Write a function that filters the `spongebobCharacters` array to find all the characters that work at the Krusty Krab. Return an array containing their names.
+
+// Superheroes in Bikini Bottom: Write a function that filters the `spongebobCharacters` array to find characters whose jobs contain the word "Superhero". Return an array containing their names.
+
+// Students and Teachers: Write a function that finds characters from the `spongebobCharacters` array who are either students or teachers. Return an array containing their names and jobs.
+
+// Characters with Specific Roles: Write a function that takes a job title as an argument and returns an array of characters from the `spongebobCharacters` array who have that exact job title.
