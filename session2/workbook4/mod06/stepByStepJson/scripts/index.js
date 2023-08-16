bb70087415f92452db681c37522542758e41a8af
Maaike <maaikejvp@hotmail.com>
2023-08-16 17:44:53 +0200

    step by step demo

---
+++ session2/workbook4/mod06/stepByStepJson/scripts/index.js
@@ -0,0 +1,20 @@
+let users = [];
+// onload
+window.onload = () => {
+    // fetch data
+    loadUsersData();
+    // assign onclick for the button
+    document.getElementById("showData").onclick = onClickShowData;
+};
+
+
+
+// function load data
+function loadUsersData() {
+
+}
+
+// function show data
+function onClickShowData() {
+
+}
