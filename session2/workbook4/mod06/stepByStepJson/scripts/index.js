4746f6f2baafa215086c8b0209a0dce5999572a9
Maaike <maaikejvp@hotmail.com>
2023-08-16 17:53:47 +0200

    done

--- session2/workbook4/mod06/stepByStepJson/scripts/index.js
+++ session2/workbook4/mod06/stepByStepJson/scripts/index.js
@@ -11,10 +11,20 @@ window.onload = () => {
 
 // function load data
 function loadUsersData() {
-
+    fetch("http://127.0.0.1:5500/session2/workbook4/mod06/stepByStepJson/data/users.json")
+        .then(result => result.json())
+        .then(data => users = data);
 }
 
 // function show data
 function onClickShowData() {
-
+    const displayData = document.getElementById("displayData");
+    // loop over users
+    for(let user of users) {
+        // create element with data from users array
+        let p = document.createElement("p");
+        p.innerHTML = user["username"];
+        // append it to div for displaying
+        displayData.appendChild(p);
+    }
 }
