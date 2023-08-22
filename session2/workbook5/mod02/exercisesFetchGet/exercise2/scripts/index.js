8da4f63bc77fc1e6dbe7b183401d017bccb22d3e
Maaike <maaikejvp@hotmail.com>
2023-08-22 15:06:18 +0200

    exercise 2

--- session2/workbook5/mod02/exercisesFetchGet/exercise2/scripts/index.js
+++ session2/workbook5/mod02/exercisesFetchGet/exercise2/scripts/index.js
@@ -7,20 +7,26 @@ window.addEventListener("load", () => {
 function populateUsersTable() {
     // call the api
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(response => response.json())  // convert to json format
-    .then(users => {
-        const usersTableBody = document.getElementById("usersTableBody");
-        usersTableBody.innerHTML = "";
-        let headers = ["id", "name", "username", "email", "phone", "website"];
-        let headerRow = usersTableBody.insertRow(-1);
-        for(let header of headers) {
-            headerRow.insertCell(-1).innerHTML = header;
-        }
-        for(let user of users) {
-            let row = usersTableBody.insertRow(-1);
-            for(let header of headers) {
-                row.insertCell(-1).innerHTML = user[header];
+        .then(response => response.json())  // convert to json format
+        .then(users => {
+            const usersTableBody = document.getElementById("usersTableBody");
+            usersTableBody.innerHTML = "";
+            // let headers = [{ id: "Id" }, { name: "Name" }, { username: "Username" }, { email: "Email" }, { phone: "Phone number" }, { website: "Website" }];
+            let headers = {
+                id: "Id",
+                name: "Name",
+                phone: "Phone"
             }
-        }
-    });
+            let headerRow = usersTableBody.insertRow(-1);
+            for (let headerProp in headers) {
+                headerRow.insertCell(-1).innerHTML = headers[headerProp];
+
+            }
+            for (let user of users) {
+                let row = usersTableBody.insertRow(-1);
+                for (let headerProp in headers) {
+                    row.insertCell(-1).innerHTML = user[headerProp];
+                }
+            }
+        });
 }
