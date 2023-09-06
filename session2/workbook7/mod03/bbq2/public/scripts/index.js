22004033f1860ce8c28dea43c1748d0071fce144
Maaike <maaikejvp@hotmail.com>
2023-09-06 21:42:52 +0200

    js

---
+++ session2/workbook7/mod03/bbq2/public/scripts/index.js
@@ -0,0 +1,32 @@
+"use strict";
+$(function () {
+    // Connect the click of each navbar hyperlink to an event
+    // handler that calls the function that creates the HTML
+    $("#homeLink").on("click", function () {
+        drawHomePage();
+    });
+    $("#orderPageLink").on("click", function () {
+        drawOrderPage();
+    });
+    $("#viewCartPageLink").on("click", function () {
+        drawCartPage();
+    });
+    // Draw the landing view when the page loads
+    drawHomePage();
+});
+function drawHomePage() {
+    // erase previous contents
+    $("#page-container").html("");
+    // define page contents
+    let pageShell = $(`
+<div class="row">
+<div class="col-md-8 mx-auto text-center">
+<h3>Home Page is Under Construction</h3>
+</div>
+</div>
+ `);
+    // add page contents to <main>
+    $("#page-container").append(pageShell);
+    // set the page title
+    $("title").html("My BBQ Restaurant");
+};
