bc406754913aa3eb0d5ae5bfe64c151c3b6dc963
Maaike <maaikejvp@hotmail.com>
2023-09-11 20:41:39 +0200

    functions

---
+++ session2/workbook8/simplemath/someFunctions.ts
@@ -0,0 +1,12 @@
+function add(nr1: number, nr2: number): number {
+    return nr1 + nr2;
+}
+ 
+let result: number = add (3, 4);
+
+function displayResult(result: number): void {
+    console.log("The result it: " + result);
+}
+
+let x = displayResult(result);
+console.log(x);
