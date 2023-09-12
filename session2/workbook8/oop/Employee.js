621ff85c7d8b7df209897bdc0dc6384bdb22381c
Maaike <maaikejvp@hotmail.com>
2023-09-12 14:44:41 +0200

    ts clas

---
+++ session2/workbook8/oop/Employee.js
@@ -0,0 +1,22 @@
+"use strict";
+class Employee {
+    // relation between id: number in parameter list and on line 6?
+    // they are not related, but, since we assign id to this.id on line 14, the types must match
+    // otherwise it won't transpile
+    constructor(id, name, jobTitle, rate) {
+        this.id = id;
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.rate = rate;
+    }
+    raisePay(percentage) {
+        this.rate = this.rate * (1 + percentage);
+    }
+    toString() {
+        return `Employee ${this.id} with name ${this.name} and ${this.jobTitle} has ${this.rate}.`;
+    }
+}
+let emp1 = new Employee(1, "Siddalee Grace", "Interior Designer", 42.00);
+console.log(emp1);
+emp1.raisePay(0.1);
+console.log(emp1);
