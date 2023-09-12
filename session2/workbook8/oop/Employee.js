a34fe158de2241bc38433d5de1bd85973ee4353f
Maaike <maaikejvp@hotmail.com>
2023-09-12 16:43:46 +0200

    objects and classes

--- session2/workbook8/oop/Employee.js
+++ session2/workbook8/oop/Employee.js
@@ -13,10 +13,12 @@ class Employee {
         this.rate = this.rate * (1 + percentage);
     }
     toString() {
+        // this.raisePay(0.2); // when you need to call a method from another method, you use the this. keyword 
         return `Employee ${this.id} with name ${this.name} and ${this.jobTitle} has ${this.rate}.`;
     }
 }
 let emp1 = new Employee(1, "Siddalee Grace", "Interior Designer", 42.00);
 console.log(emp1);
 emp1.raisePay(0.1);
-console.log(emp1);
+console.log(emp1.toString());
+console.log(emp1.constructor.name);
--- session2/workbook8/oop/Employee.ts
+++ session2/workbook8/oop/Employee.ts
@@ -1,10 +1,8 @@
-
-
 class Employee {
-    public id: number;
+    private id: number;
     public name: string;
     public jobTitle: string;
-    public rate: number;
+    private rate: number;
 
     // relation between id: number in parameter list and on line 6?
     // they are not related, but, since we assign id to this.id on line 14, the types must match
@@ -21,8 +19,10 @@ class Employee {
     }
 
     toString(): string {
+        // this.raisePay(0.2); // when you need to call a method from another method, you use the this. keyword 
         return `Employee ${this.id} with name ${this.name} and ${this.jobTitle} has ${this.rate}.`;
     }
+
 }
 
 let emp1 = new Employee(
@@ -30,4 +30,10 @@ let emp1 = new Employee(
 );
 console.log(emp1);
 emp1.raisePay(0.1);
-console.log(emp1);
+console.log(emp1.toString());
+
+// way to get the name of the class starting from the object / instance
+console.log(emp1.constructor.name);
+
+
+
