5136a475355c75d7d5cb21012502f832ad5d79d7
Maaike <maaikejvp@hotmail.com>
2023-09-26 19:11:42 +0200

    active link

--- session3/lists/src/app/app.module.ts
+++ session3/lists/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const appRoutes: Routes = [
-  { path: "", component: HomeComponent},
-  { path: "contact", component: ContactUsComponent }
+  { path: "home", component: HomeComponent},
+  { path: "contact", component: ContactUsComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" }
+
 ];
 @NgModule({
   declarations: [
